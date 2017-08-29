51bfda7252074c23fb81a708ba4a73dc8e1febc8
Adam Parlane <adamsp743@hotmail.com>
2017-08-29 20:29:56 +1200

    added scale calculation

--- ESE-Assignment1-1304788/ReferenceLine.py
+++ ESE-Assignment1-1304788/ReferenceLine.py
@@ -6,7 +6,13 @@ class ReferenceLine():
         self.centres = redPins.centres
 
     def calculateScaleFactor(self):
+        import cv2
         self._drawReferenceLine()
+        if (len(self.centres) > 1):
+            differenceX = self.centres[-1][-2] - self.centres[-2][-2]
+            differenceY = self.centres[-1][-1] - self.centres[-2][-1]
+            distance = (differenceX**2 + differenceY**2)**0.5
+            print distance
 
     def _drawReferenceLine(self):
         import cv2
